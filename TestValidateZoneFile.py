0a4786fbe89509b5730e5d330490aabbb1351048
Che-Huai Lin <lzh9102@gmail.com>
2014-02-23 22:45:41 -0500

    Rename validateSomething() to isCorrectSomething() to avoid confusion

--- TestValidateZoneFile.py
+++ TestValidateZoneFile.py
@@ -3,28 +3,28 @@
 import unittest
 from ValidateZoneFile import *
 
-class TestValidateZoneFile(unittest.TestCase):
+class TestIsCorrectZoneFile(unittest.TestCase):
 
-    def testValidateHostName(self):
-        self.assertTrue(validateHostName("cc.cmu.edu"))
-        self.assertTrue(validateHostName("a_b.cmu.edu")) # allow underscore
-        self.assertTrue(validateHostName("a-b.cmu.edu")) # allow hyphen
-        self.assertFalse(validateHostName("cc.cmu .edu")) # spaces not allowed
-        self.assertFalse(validateHostName("cc.cmu.edu*")) # invalid char
+    def testIsCorrectHostName(self):
+        self.assertTrue(isCorrectHostName("cc.cmu.edu"))
+        self.assertTrue(isCorrectHostName("a_b.cmu.edu")) # allow underscore
+        self.assertTrue(isCorrectHostName("a-b.cmu.edu")) # allow hyphen
+        self.assertFalse(isCorrectHostName("cc.cmu .edu")) # spaces not allowed
+        self.assertFalse(isCorrectHostName("cc.cmu.edu*")) # invalid char
 
-    def testValidateInteger(self):
-        self.assertTrue(validateInteger("0"))
-        self.assertTrue(validateInteger("12"))
-        self.assertTrue(validateInteger("123"))
-        self.assertFalse(validateInteger("023"))
-        self.assertFalse(validateInteger("0a3"))
+    def testIsCorrectInteger(self):
+        self.assertTrue(isCorrectInteger("0"))
+        self.assertTrue(isCorrectInteger("12"))
+        self.assertTrue(isCorrectInteger("123"))
+        self.assertFalse(isCorrectInteger("023"))
+        self.assertFalse(isCorrectInteger("0a3"))
 
-    def testValidateIPv4(self):
-        self.assertTrue(validateIPv4("192.168.1.1"))
-        self.assertTrue(validateIPv4("255.255.255.255"))
-        self.assertTrue(validateIPv4("255.0.0.0"))
-        self.assertFalse(validateIPv4("255.256.255.255"))
-        self.assertFalse(validateIPv4("abc.255.255.255"))
+    def testIsCorrectIPv4(self):
+        self.assertTrue(isCorrectIPv4("192.168.1.1"))
+        self.assertTrue(isCorrectIPv4("255.255.255.255"))
+        self.assertTrue(isCorrectIPv4("255.0.0.0"))
+        self.assertFalse(isCorrectIPv4("255.256.255.255"))
+        self.assertFalse(isCorrectIPv4("abc.255.255.255"))
 
 if __name__ == "__main__":
     unittest.main()
--- ValidateZoneFile.py
+++ ValidateZoneFile.py
@@ -2,7 +2,7 @@
 
 import re
 
-def validateHostName(name):
+def isCorrectHostName(name):
     if name.find(' ') != -1: # name contains spaces
         return False
     for part in name.split('.'): # split the parts of the server name
@@ -10,15 +10,15 @@ def validateHostName(name):
             return False
     return True
 
-def validateInteger(s):
+def isCorrectInteger(s):
     return re.match("^(0|[1-9][0-9]*)$", s) != None
 
-def validateIPv4(addr):
+def isCorrectIPv4(addr):
     parts = addr.split('.')
     if len(parts) != 4: # ipv4 contains 4 numbers
         return False
     for part in parts:
-        if not validateInteger(part): # check if it's a number
+        if not isCorrectInteger(part): # check if it's a number
             return False
         if int(part) > 255: # check if the number is within 0~255
             return False
